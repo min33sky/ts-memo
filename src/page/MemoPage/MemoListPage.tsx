95e3e7aade472d59ca4a2be67d1f596022e0b0ba
min33sky <min33sky@naver.com>
2020-05-06 01:41:06 +0900

    Add actionTypes, actionCreators & Update reducer

--- src/page/MemoPage/MemoListPage.tsx
+++ src/page/MemoPage/MemoListPage.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import { Memo } from '../../model';
+import Layout from '../../components/Layout';
+import Sidebar, {
+  SidebarBackButton,
+  SidebarTitle,
+} from '../../components/Sidebar';
+import Main from '../../components/Main';
+import MemoRouter from '../../routes/Memo';
+import { List, ListItem } from '../../components/List';
+import { SideMemuButton } from '../../components/Button';
+import AddMemoButton from '../../components/AddMemoButton';
 
 interface MemoListPageProps {
   memos: Memo[];
 }
 
 function MemoListPage({ memos }: MemoListPageProps) {
-  return <div>메모리스트페이지</div>;
+  return (
+    <Layout>
+      <Sidebar>
+        <SidebarBackButton to='/' />
+        <SidebarTitle>메모</SidebarTitle>
+        {memos.length > 0 && <MemoList memos={memos} />}
+      </Sidebar>
+      <Main>
+        <AddMemoButton />
+        <MemoRouter />
+      </Main>
+    </Layout>
+  );
+}
+
+function MemoList({ memos }: MemoListPageProps) {
+  const memoTitle = (content: string) => content.substr(0, 15);
+
+  return (
+    <List>
+      {memos.map((memo, idx) => (
+        <ListItem key={idx} first={idx === 0}>
+          <SideMemuButton to={`/memo/${memo.id}`}>
+            {memoTitle(memo.content)}
+          </SideMemuButton>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
 
 export default MemoListPage;
--- src/reducers/memo.tsx
+++ src/reducers/memo.tsx
@@ -3,6 +3,12 @@ import { Memo } from '../model';
 // ***** Action Type ***** //
 
 export const FETCH_MEMO_LIST = 'memo/FETCH_MEMO_LIST' as const;
+export const FETCH_DELETED_MEMO_LIST = 'memo/FETCH_DELETED_MEMO_LIST' as const;
+export const FETCH_MEMO = 'memo/FETCH_MEMO' as const;
+export const FETCH_DELETED_MEMO = 'memo/FETCH_DELETED_MEMO' as const;
+export const ADD_MEMO = 'memo/ADD_MEMO' as const;
+export const DELETE_MEMO = 'memo/DELETE_MEMO' as const;
+export const RESTORE_MEMO = 'memo/RESTORE_MEMO' as const;
 
 // ***** Actino Creator ***** //
 
@@ -15,30 +21,120 @@ export const fetchMemoList = (memos: Memo[]) => ({
   payload: memos,
 });
 
+export const fetchDeletedMemoList = (memos: Memo[]) => ({
+  type: FETCH_DELETED_MEMO_LIST,
+  payload: memos,
+});
+
+export const fetchMemo = (memo: Memo) => ({
+  type: FETCH_MEMO,
+  payload: memo,
+});
+
+export const fetchDeletedMemo = (memo: Memo) => ({
+  type: FETCH_DELETED_MEMO,
+  payload: memo,
+});
+
+export const addMemo = (memo: Memo) => ({
+  type: ADD_MEMO,
+  payload: memo,
+});
+
+export const deleteMemo = (id: number) => ({
+  type: DELETE_MEMO,
+  payload: id,
+});
+
+export const restoreMemo = (id: number) => ({
+  type: RESTORE_MEMO,
+  payload: id,
+});
+
 // ***** Action Type ***** //
 
-type MemoAction = ReturnType<typeof fetchMemoList>;
+type MemoAction =
+  | ReturnType<typeof fetchMemoList>
+  | ReturnType<typeof fetchDeletedMemoList>
+  | ReturnType<typeof fetchMemo>
+  | ReturnType<typeof fetchDeletedMemo>
+  | ReturnType<typeof addMemo>
+  | ReturnType<typeof deleteMemo>
+  | ReturnType<typeof restoreMemo>;
 
 // ***** State ***** //
 
 export interface MemoState {
   memos: Memo[];
+  deletedMemos: Memo[];
 }
 
 const initialState: MemoState = {
   memos: [],
+  deletedMemos: [],
 };
 
 // ***** Reducer ***** //
 
 function memoReducer(state = initialState, action: MemoAction): MemoState {
   switch (action.type) {
-    case FETCH_MEMO_LIST:
-      const fetchedMemos = action.payload;
+    case FETCH_MEMO_LIST: {
       return {
         ...state,
-        memos: fetchedMemos.map((memo) => ({ ...memo })),
+        memos: action.payload,
       };
+    }
+
+    case FETCH_DELETED_MEMO_LIST: {
+      return {
+        ...state,
+        deletedMemos: action.payload,
+      };
+    }
+
+    case FETCH_MEMO: {
+      return {
+        ...state,
+        memos: state.memos.map((memo) => {
+          if (memo.id !== action.payload.id) return memo;
+          return { ...action.payload };
+        }),
+      };
+    }
+
+    case FETCH_DELETED_MEMO: {
+      return {
+        ...state,
+        deletedMemos: state.deletedMemos.map((memo) => {
+          if (memo.id !== action.payload.id) return memo;
+          return { ...action.payload };
+        }),
+      };
+    }
+
+    case ADD_MEMO: {
+      return {
+        ...state,
+        memos: [action.payload, ...state.memos],
+      };
+    }
+
+    case DELETE_MEMO: {
+      return {
+        ...state,
+        memos: state.memos.filter((memo) => memo.id !== action.payload),
+      };
+    }
+
+    case RESTORE_MEMO: {
+      // if (!action.payload) return state;
+      return {
+        ...state,
+        deletedMemos: state.deletedMemos.filter(
+          (memo) => memo.id !== action.payload,
+        ),
+      };
+    }
 
     default:
       return state;
