f92ed9ab40e37471323dad624f13bb19a45427a4
min33sky <min33sky@naver.com>
2020-07-16 17:56:33 +0900

    merge saga

--- src/containers/MemoListContainer.tsx
+++ src/containers/MemoListContainer.tsx
@@ -11,15 +11,9 @@ function MemoListContainer() {
   const { memos } = useSelector((state: RootState) => state.memo);
   const dispatch = useDispatch();
 
-  /**
-   * TODO: 스토어에 메모 리스트 업데이트
-   */
-
   useEffect(() => {
     dispatch(fetchMemoListRequest());
   }, [dispatch]);
-
-  console.log('********* 실행 **********', memos);
 
   return <MemoListPage memos={memos} />;
 }
